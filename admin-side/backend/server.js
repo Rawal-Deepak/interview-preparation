51e68d0e214f8051fcffc1f7bd8832a7389412a2
Deepak Rawal <deepakrawal3736@gmail.com>
2024-10-27 16:59:20 +0530

    Registration Form complete with database

--- admin-side/backend/server.js
+++ admin-side/backend/server.js
@@ -1,11 +1,35 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const connectDB = require("./config/db");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
 const app = express();
 const port = 5000;
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
 
 connectDB();
+const registerData = new mongoose.Schema({
+  username: String,
+  email: String,
+  password: String,
+});
+const FormData = mongoose.model("Admin-side-users", registerData);
 
-app.use(express.json());
+app.post("/register-admin-data", async (req, res) => {
+  try {
+    const formData = new FormData({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    await formData.save();
+    res.send("Data Save Succefully!");
+  } catch (err) {
+    res.status(500).send("Failed to save data \n" + err);
+  }
+});
 
-app.get("/", (req, res) => res.send("API running..."));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- admin-side/src/components/Register/Register.js
+++ admin-side/src/components/Register/Register.js
@@ -1,6 +1,35 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Register() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRegister = async (event) => {
+    event.preventDefault();
+    const formData = {
+      username: username,
+      email: email,
+      password: password,
+    };
+    try {
+      const response = await fetch(
+        "http://localhost:5000/register-admin-data",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+      console.log(response);
+    } catch (error) {
+      console.error("Error submitting Form: ", error);
+    }
+  };
+
   return (
     <>
       <div className="Login flex justify-center items-center w-screen h-screen bg-gray-100">
@@ -16,9 +45,12 @@ function Register() {
               <input
                 type="text"
                 name="username"
+                id="username"
+                value={username}
                 placeholder="Enter Your Username"
                 className="block shadow-sm py-1.5 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-[#5652B7] focus:outline-none h-10 w-72 px-2 rounded-md"
                 required
+                onChange={(e) => setUsername(e.target.value)}
               />
             </div>
 
@@ -30,9 +62,12 @@ function Register() {
               <input
                 type="text"
                 name="email"
+                id="email"
+                value={email}
                 placeholder="Enter Your Email"
                 className="block shadow-sm py-1.5 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-[#5652B7] focus:outline-none h-10 w-72 px-2 rounded-md"
                 required
+                onChange={(e) => setEmail(e.target.value)}
               />
             </div>
 
@@ -44,8 +79,12 @@ function Register() {
               <input
                 type="password"
                 name="password"
+                id="password"
+                value={password}
                 placeholder="Enter Your Password"
                 className="block shadow-sm py-1.5 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-[#5652B7] focus:outline-none h-10 w-72 px-2 rounded-md"
+                required
+                onChange={(e) => setPassword(e.target.value)}
               />
             </div>
 
@@ -57,6 +96,7 @@ function Register() {
               <input
                 type="password"
                 name="confirm_password"
+                id="confirm-password"
                 placeholder="Enter Your Confirmation Password"
                 className="block shadow-sm py-1.5 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-[#5652B7] focus:outline-none h-10 w-72 px-2 rounded-md"
               />
@@ -68,6 +108,7 @@ function Register() {
                 name="submit"
                 value="Sign up"
                 className="w-28 h-10 bg-[#5652B7] shadow-sm text-white rounded-md hover:bg-[#6461BD] hover:transition-all hover:w-[6.5rem] hover:h-[2.3rem] cursor-pointer"
+                onClick={handleRegister}
               />
             </div>
             <div>
