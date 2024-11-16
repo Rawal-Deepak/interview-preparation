b96540d4a3b3529c5fde73350e656ade1f6b50a2
Deepak Rawal <deepakrawal3736@gmail.com>
2024-11-17 02:54:53 +0530

    Structured Format

--- admin-side-backend/routes/loginRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const loginController = require("../controllers/loginController");
-
-const router = express.Router();
-
-router.route("/login-admin-user").post(loginController.loginAdmin);
-
-module.exports = router;
